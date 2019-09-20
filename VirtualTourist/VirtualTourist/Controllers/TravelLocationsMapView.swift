8c8f6b965d5a046bbd11d145c1adf7ade837e798
Mahreen Azam <Mahreen.azam@willowtreeapps.com>
2019-09-20 01:37:06 -0400

    Started making map view persistent

--- VirtualTourist/VirtualTourist/Controllers/TravelLocationsMapView.swift
+++ VirtualTourist/VirtualTourist/Controllers/TravelLocationsMapView.swift
@@ -20,11 +20,21 @@ class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
         
         mapView.delegate = self
         
+        if let didMapChange = UserDefaults.standard.value(forKey: "mapChanges") {
+            if didMapChange as! Bool {
+                print("Changes") // Update this to set map to updated version
+                mapView.centerCoordinate.latitude = UserDefaults.standard.value(forKey: "mapLatitude") as! Double
+                mapView.centerCoordinate.longitude = UserDefaults.standard.value(forKey: "mapLongitude") as! Double
+            }
+        } else {
+            UserDefaults.standard.setValue(false, forKey: "mapChanges")
+            print("first launch")
+        }
+        
+        // Adds long press recognizer which will add pins to the map when recognized
         let longPress: UILongPressGestureRecognizer = UILongPressGestureRecognizer()
         longPress.addTarget(self, action: #selector(recognizeLongPress(_:)))
-        
         mapView.addGestureRecognizer(longPress)
-        
     }
     
     @objc private func recognizeLongPress(_ sender: UILongPressGestureRecognizer) {
@@ -33,6 +43,8 @@ class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
             return
         }
         
+        var pinArray = [MKPointAnnotation]()
+        
         let location = sender.location(in: mapView)
         let myCoordinate: CLLocationCoordinate2D = mapView.convert(location, toCoordinateFrom: mapView)
         
@@ -40,6 +52,7 @@ class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
         myPin.coordinate = myCoordinate
         
         mapView.addAnnotation(myPin)
+        pinArray.append(myPin)
     }
     
     // MARK: - MKMapViewDelegate
@@ -62,60 +75,11 @@ class TravelLocationsMapView: UIViewController, MKMapViewDelegate {
         return pinView
     }
     
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        UserDefaults.standard.setValue(true, forKey: "mapChanges")
+        UserDefaults.standard.setValue(Double(mapView.centerCoordinate.latitude.description), forKey: "mapLatitude")
+        UserDefaults.standard.setValue(Double(mapView.centerCoordinate.longitude.description), forKey: "mapLongitude")
         print("Hello")
-    }
-    
-    // MARK: - Sample Data
-    
-    func hardCodedLocationData() -> [[String : Any]] {
-        return  [
-            [
-                "createdAt" : "2015-02-24T22:27:14.456Z",
-                "firstName" : "Jessica",
-                "lastName" : "Uelmen",
-                "latitude" : 28.1461248,
-                "longitude" : -82.75676799999999,
-                "mapString" : "Tarpon Springs, FL",
-                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
-                "objectId" : "kj18GEaWD8",
-                "uniqueKey" : 872458750,
-                "updatedAt" : "2015-03-09T22:07:09.593Z"
-            ], [
-                "createdAt" : "2015-02-24T22:35:30.639Z",
-                "firstName" : "Gabrielle",
-                "lastName" : "Miller-Messner",
-                "latitude" : 35.1740471,
-                "longitude" : -79.3922539,
-                "mapString" : "Southern Pines, NC",
-                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
-                "objectId" : "8ZEuHF5uX8",
-                "uniqueKey" : 2256298598,
-                "updatedAt" : "2015-03-11T03:23:49.582Z"
-            ], [
-                "createdAt" : "2015-02-24T22:30:54.442Z",
-                "firstName" : "Jason",
-                "lastName" : "Schatz",
-                "latitude" : 37.7617,
-                "longitude" : -122.4216,
-                "mapString" : "18th and Valencia, San Francisco, CA",
-                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
-                "objectId" : "hiz0vOTmrL",
-                "uniqueKey" : 2362758535,
-                "updatedAt" : "2015-03-10T17:20:31.828Z"
-            ], [
-                "createdAt" : "2015-03-11T02:48:18.321Z",
-                "firstName" : "Jarrod",
-                "lastName" : "Parkes",
-                "latitude" : 34.73037,
-                "longitude" : -86.58611000000001,
-                "mapString" : "Huntsville, Alabama",
-                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
-                "objectId" : "CDHfAy8sdp",
-                "uniqueKey" : 996618664,
-                "updatedAt" : "2015-03-13T03:37:58.389Z"
-            ]
-        ]
     }
 }
 
---
+++ VirtualTourist/VirtualTourist/Model/DataController.swift
@@ -0,0 +1,32 @@
+//
+//  DataController.swift
+//  VirtualTourist
+//
+//  Created by Mahreen Azam on 9/18/19.
+//  Copyright © 2019 Mahreen Azam. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer:NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion?()
+        }
+    }
+    
+}
