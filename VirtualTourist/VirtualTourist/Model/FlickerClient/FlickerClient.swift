3d3be96aff47de243d0a99583368cd36a928f1da
Mahreen Azam <azam03m@gmail.com>
2019-09-25 16:03:35 -0400

    Added a collection view instead of an image on photo album VC. Created Flicker Client file.

---
+++ VirtualTourist/VirtualTourist/Model/FlickerClient/FlickerClient.swift
@@ -0,0 +1,20 @@
+//
+//  FlickerClient.swift
+//  VirtualTourist
+//
+//  Created by Mahreen Azam on 9/25/19.
+//  Copyright © 2019 Mahreen Azam. All rights reserved.
+//
+
+import Foundation
+
+class FlickerClient {
+    
+    struct Auth {
+        var sessionID = ""
+    }
+    
+    struct Endpoints {
+        var endpoint = ""
+    }
+}
