2a4a3cc712600c77fd8df4496214fd436ba52062
Mahreen Azam <azam03m@gmail.com>
2019-09-26 14:55:55 -0400

    Started adding collection view methods

--- VirtualTourist/VirtualTourist/Controllers/PhotoAlbumView.swift
+++ VirtualTourist/VirtualTourist/Controllers/PhotoAlbumView.swift
@@ -12,7 +12,8 @@ import MapKit
 class PhotoAlbumView: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var photoAlbum: UIImageView!
+    
+    @IBOutlet weak var photoAlbumView: UICollectionView!
     
     // MARK: Global Variables
     var centerCoordinate: CLLocationCoordinate2D!
@@ -39,7 +40,7 @@ class PhotoAlbumView: UIViewController, MKMapViewDelegate {
         let annotation = MKPointAnnotation()
         annotation.coordinate = centerCoordinate
         
-        let mapArea = MKCoordinateRegion(center: centerCoordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        let mapArea = MKCoordinateRegion(center: centerCoordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
         
         DispatchQueue.main.async {
             self.mapView.addAnnotation(annotation)
@@ -56,5 +57,33 @@ class PhotoAlbumView: UIViewController, MKMapViewDelegate {
             }
         }
     }
-
 }
+
+//MARK: UICollectionViewDelegate
+extension PhotoAlbumView: UICollectionViewDelegate{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FlickerImageCell", for: indexPath)
+        return cell
+    }
+}
+
+//MARK: UICollectionViewDataSource
+extension PhotoAlbumView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+//MARK: UICollectionViewDelegateFlowLayout
+extension PhotoAlbumView: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+}
+
+
